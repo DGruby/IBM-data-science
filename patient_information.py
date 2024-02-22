9895cccfe51e76a0437049a7bab6298052e1d4fa
DGruby <149113835+DGruby@users.noreply.github.com>
2024-02-22 15:37:54 +0100

    Add files via upload

---
+++ patient_information.py
@@ -0,0 +1,14 @@
+'''
+class Patient(name, age, disease)
+    def __init__(self)
+'''
+
+def main():
+    name = input("Imię i nazwisko: ")
+    age = input("Wiek: ")
+    diesease = input("Choroba: ")
+    print(f"DANE PACJENTA\n\n\tImię i nazwisko: {name}\n\tWiek: {age}\n\tChoroba: {diesease}")
+
+if __name__ == "__main__":
+    main()
+    
